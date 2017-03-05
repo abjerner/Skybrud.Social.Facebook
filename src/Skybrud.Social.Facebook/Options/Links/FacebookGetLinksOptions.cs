968f1e7f0f7a2ef6c69add84a8ea89a8587fae83
Anders Bjerner <abjerner@skybrud.dk>
2017-03-05 15:55:37 +0100

    The links endpoint/methods was deprecated from v2.4 and newer versions of the Graph API

--- src/Skybrud.Social.Facebook/Options/Links/FacebookGetLinksOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿using Skybrud.Social.Facebook.Options.Common.Pagination;
-
-namespace Skybrud.Social.Facebook.Options.Links {
-
-    public class FacebookGetLinksOptions : FacebookCursorBasedPaginationOptions { }
-
-}
