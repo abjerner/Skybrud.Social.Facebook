d169be15ec67f64fd2645b13e2aae2a249ea8df3
Anders Bjerner <abjerner@skybrud.dk>
2017-03-23 22:13:37 +0100

    Added missing file to the repository

---
+++ src/Skybrud.Social.Facebook/Responses/Debug/FacebookDebugTokenResponse.cs
@@ -0,0 +1,40 @@
+using Skybrud.Social.Facebook.Objects.Debug;
+using Skybrud.Social.Http;
+
+namespace Skybrud.Social.Facebook.Responses.Debug {
+
+    /// <summary>
+    /// Class representing a response for getting information about a given access token.
+    /// </summary>
+    public class FacebookDebugTokenResponse : FacebookResponse<FacebookDebugToken> {
+
+        #region Constructors
+
+        private FacebookDebugTokenResponse(SocialHttpResponse response) : base(response) {
+
+            // Validate the response
+            ValidateResponse(response);
+
+            // Parse the response body
+            Body = ParseJsonObject(response.Body, FacebookDebugToken.Parse);
+
+        }
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Parses the specified <paramref name="response"/> into an instance of <see cref="FacebookDebugTokenResponse"/>.
+        /// </summary>
+        /// <param name="response">The instance of <see cref="SocialHttpResponse"/> representing the raw response.</param>
+        /// <returns>An instance of <see cref="FacebookDebugTokenResponse"/> representing the response.</returns>
+        public static FacebookDebugTokenResponse ParseResponse(SocialHttpResponse response) {
+            return response == null ? null : new FacebookDebugTokenResponse(response);
+        }
+
+        #endregion
+
+    }
+
+}
